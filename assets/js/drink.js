09b4a4d2ffbb6d064e38285a656266d1de7e42fc
josik <joe.code.mckinney@gmail.com>
2023-04-17 11:19:50 -0500

    testing fetch API functions

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -1,31 +1,35 @@
 //Joe's fetch API work for Drinks
-const API_BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
+const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
+
+getRandomCocktail();
+searchCocktailsByName('Jitterbug');
 
 // Fetch a random cocktail
 async function getRandomCocktail() {
-  const response = await fetch(`${API_BASE_URL}random.php`);
+  const response = await fetch(`${API_URL}random.php`);
   const data = await response.json();
   console.log(data.drinks[0])
   return data.drinks[0];
 }
 
-// Search cocktails by name
-// async function searchCocktailsByName(name) {
-//   const response = await fetch(`${API_BASE_URL}search.php?s=${name}`);
-//   const data = await response.json();
-//   return data.drinks;
-// }
+//Search cocktails by name
+async function searchCocktailsByName(name) {
+  const response = await fetch(`${API_URL}search.php?s=${name}`);
+  const data = await response.json();
+  console.log(data.drinks);
+  return data.drinks;
+}
 
 // Search cocktails by ingredient
-// async function searchCocktailsByIngredient(ingredient) {
-//   const response = await fetch(`${API_BASE_URL}filter.php?i=${ingredient}`);
-//   const data = await response.json();
-//   return data.drinks;
-// }
+async function searchCocktailsByIngredient(ingredient) {
+  const response = await fetch(`${API_URL}filter.php?i=${ingredient}`);
+  const data = await response.json();
+  return data.drinks;
+}
 
 // Get cocktail details by ID
 // async function getCocktailDetailsById(id) {
-//   const response = await fetch(`${API_BASE_URL}lookup.php?i=${id}`);
+//   const response = await fetch(`${API_URL}lookup.php?i=${id}`);
 //   const data = await response.json();
 //   return data.drinks[0];
 // }
