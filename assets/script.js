74e1293a0047e599d930e9dc59a69d5e8a8ae147
Ki Sar <rxs291@yahoo.com>
2023-04-17 10:41:20 -0500

    working scripts

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,18 @@
 
 
 var testing = "http://api.openweathermap.org/geo/1.0/direct?q=austin&limit=1&appid=31fbadef98a417ef6f0e39d36c133d27" 
+
+var testing2 = "www.themealdb.com/api/json/v1/1/search.php?s=chicken&key=1"
  
+console.log(testing2)
 
-
-fetch(testing)
+fetch(testing2)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {   
+    console.log(data)
+
   lon = data[0].lon
   lat =  data[0].lat  
 
