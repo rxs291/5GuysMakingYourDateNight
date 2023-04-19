8522125487079d0bae6960146730a7d3ffe47982
Ki Sar <rxs291@yahoo.com>
2023-04-19 10:00:28 -0500

    scripts

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,10 @@ var youtubeLink = '';
 var thumbnail = '';
 var instructions = '';
 var testingContainer = document.querySelector(".testingContainer")
+var menu1 = document.querySelector("#menu1");
+var menu2 = document.querySelector("#menu2");
+var myButton = document.querySelector(".searchBtn");
+var categoriesFood = ["Breakfast", "Side","Starter","Dessert", "Beef","Chicken","Pork","Lamb","Goat","Pasta","Seafood","Vegetarian","Vegan","Miscelaneous"]
  
 
 ////this pulls meals by first letter, not for functional use yet
@@ -27,26 +31,114 @@ var requestCatergorys = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
 
 
+///////THIS IS THE EVENT LISTNER TO GRAB MENU VALUES ON CLICK, THEN USE THE APPROPRIATE API ACCORDINGLY 
+myButton.addEventListener("click", function() {
+    var menu1Value = menu1.value;
+    var menu2Value = menu2.value;
+     
+    console.log(categoriesFood[menu1Value - 1], categoriesFood[menu2Value]);
+
+    if (categoriesFood[menu1Value - 1] === "Random"){
+        var container = document.querySelector('.testingContainer');
+        container.innerHTML = "";
+        mealsByName(randomMeal)
+    }else if (menu1Value != "0" ){
+        var container = document.querySelector('.testingContainer');
+        container.innerHTML = ""; 
+        requestCatergoryList = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=' + categoriesFood[menu1Value - 1];
+        console.log(requestCatergoryList)
+        requestFoodList(requestCatergoryList);
+
+    } else {
+        console.log("Error")
+    }
+});
 
 
 
-fetch(requestCatergoryList)
+///THIS FUNCTION WILL PULL THE LIST OF OPTIONS FOR THE USER TO PICK ON DEPENDING ON THE FOOD CATERGORY.
+function requestFoodList(x){
+fetch(x)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {   
 
     console.log(data)   
+    for(var i = 0; i<data.meals.length; i++){
+
+        //////HERE IS WHERE WE WILL MAKE THE ID CARDS WITH INFORMATION PURGED FROM THE API
+        mealName = data.meals[i].strMeal          ////THIS PULLS NAME 
+        youtubeLink = data.meals[i].strYoutube      //// THIS PULLS YOUTUBE LINK 
+        thumbnail = data.meals[i].strMealThumb     /// THIS PULLS THUMBNAIL  
+
+        console.log(data.meals[i].strMeal) 
+        console.log(data.meals[i].strMealThumb) 
+        
+        testingContainer.style.display = "flex" ;
+        testingContainer.style.flexWrap = "wrap" ;
+
+        ///card container 
+        var card = document.createElement('div');
+        card.classList.add('card');
+        card.style.maxWidth = "50%";
+        card.style.maxHeight = "50%"; 
+        card.style.backgroundColor = 'transparent'  
+        ////card body
+        var cardBody = document.createElement('div');
+        cardBody.style.border = "thick solid";
+        
+        // cardBody.width = "200";
+        // cardBody.height = "200"; 
+        cardBody.style.maxWidth = "50%";
+        cardBody.style.maxHeight = "100%";
+        cardBody.style.display = "block";
+        cardBody.style.margin = "0 auto";
+        cardBody.style.textAlign = "center"
+        cardBody.style.borderRadius = "25px";  
+        cardBody.style.overflow = "hidden"; 
+        // cardBody.style.display = "flex";
+        // cardBody.style.flexDirection = "column";
+
+        /////image element
+        var imageFood = document.createElement('img'); 
+        imageFood.setAttribute("src", thumbnail) ; 
+        imageFood.style.maxWidth = "50%";
+        imageFood.style.width = "100%";
+        imageFood.style.maxHeight = "50%"; 
+        imageFood.style.borderRadius = "19px";
+        // imageFood.style.objectPosition = "center";
+        // imageFood.style.display = "block";
+        // imageFood.style.margin = "auto";
+        imageFood.style.border = "thin solid"
+
+        var cardMealName = document.createElement("h5");
+        var link = document.createElement("a");
+        link.setAttribute("href", "#");
+        link.style.color = "black";
+        cardMealName.appendChild(link);
+        link.textContent = mealName;  
+
+        cardBody.appendChild(imageFood);
+        cardBody.appendChild(cardMealName); 
+        
+
+        card.appendChild(cardBody);
+        testingContainer.appendChild(card); 
+ 
+
+}
 }
 )
+}
 
 
  
 
 
-async function mealsByName(){  
+async function mealsByName(x){  
  
-fetch(randomMeal)
+fetch(x)
 .then(function (response) {
   return response.json();
 })
@@ -81,7 +173,7 @@ fetch(randomMeal)
         var cardBody = document.createElement('div')
 
 
-        for (const key in data.meals[i]){   
+        for (var key in data.meals[i]){   
              
  
 
@@ -110,16 +202,23 @@ fetch(randomMeal)
         ///card container 
         var card = document.createElement('div');
         card.classList.add('card');
+        card.style.maxWidth = "50%";
+        card.style.maxHeight = "50%"; 
+        card.style.backgroundColor = 'transparent'
+        card.style.textOverflow ="clip";
+        card.style.whiteSpace = "nowrap";
         ////card body
         var cardBody = document.createElement('div');
         cardBody.style.border = "thick solid";
+        
         // cardBody.width = "200";
         // cardBody.height = "200"; 
         cardBody.style.maxWidth = "50%";
         cardBody.style.maxHeight = "50%";
         cardBody.style.display = "block";
         cardBody.style.margin = "0 auto";
-        cardBody.style.borderRadius = "25px";
+        cardBody.style.textAlign = "center"
+        cardBody.style.borderRadius = "25px";  
         // cardBody.style.display = "flex";
         // cardBody.style.flexDirection = "column";
 
@@ -129,7 +228,7 @@ fetch(randomMeal)
         imageFood.style.maxWidth = "100%";
         imageFood.style.width = "100%";
         imageFood.style.maxHeight = "50%"; 
-        imageFood.style.borderRadius = "25px";
+        imageFood.style.borderRadius = "19px";
         // imageFood.style.objectPosition = "center";
         // imageFood.style.display = "block";
         // imageFood.style.margin = "auto";
